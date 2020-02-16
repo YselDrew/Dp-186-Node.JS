8ce5a72252befdc4f549f6422896ab4e57f40d37
Andrii <andryshonochek0604@gmail.com>
2020-02-16 20:37:43 +0200

    Add ability to decode few secret sentences

--- lesson04HW/task3/script.js
+++ lesson04HW/task3/script.js
@@ -1,37 +1,70 @@
 function decodeMessage(message) {
-  let keyIndex = 0;
+  const wordsArr = getArrayOfWords(message);
+  const secretSentences = getSecretSentences(wordsArr);
 
-  const sentences = message.split(/\? |\. |! /);
+  const secretMessage = secretSentences.join(' ');
 
-  const splittedSenteces = sentences.map(item => {
-    return item.split(/, | |: /);
-  })
-
-  const keySentence = splittedSenteces[keyIndex].map(item => {
-    return item.replace(/'|"/g, "");
-  })
-
-  let key = keySentence.map(item => item.length - 1);
-  let keyWord;
-
-  const decodedMessage = [];
-
-  for (let i = 1; i <= key.length; i++) {
-    keyWord = splittedSenteces[i][key[i - 1]];
-    decodedMessage.push(keyWord);
-  }
-
-  const decodedString = sentenceToString(decodedMessage);
-  return decodedString;
+  return secretMessage;
 }
 
-function sentenceToString(arr) {
+
+function getArrayOfWords(message) {
+  const sentences = message.split(/\? |\. |! /);
+  const words = sentences.map(item => item.split(/, | |: /));
+
+  return words;
+}
+
+
+function getSecretSentences(wordsArr) {
+  const secretMessageArr = [];
+  while (wordsArr.length > 0) {
+    let key = getKey(wordsArr);
+    let wordNumber;
+    let sentenceNumber;
+
+    const secretSentence = [];
+    const decodedPart = key.length + 1;
+
+    for (let i = 1; i <= key.length; i++) {
+      sentenceNumber = i;
+      wordNumber = key[i - 1];
+
+      secretSentence.push(wordsArr[sentenceNumber][wordNumber]);
+    }
+
+    const secretSentenceStr = transformToString(secretSentence);
+    secretMessageArr.push(secretSentenceStr);
+
+    wordsArr = wordsArr.slice(decodedPart);
+  } 
+  return secretMessageArr;
+}
+
+
+function getKey(wordsArr) {
+  const keySentence = wordsArr[0].map(item => item.replace(/'|"/g, ""));
+  const key = keySentence.map(item => item.length - 1);
+
+  return key;
+}
+
+
+function transformToString(arr) {
   const str = arr.join(' ').replace(/"/g, '');
   const strCapitalized = str.charAt(0).toUpperCase() + str.slice(1) + '.';
+
   return strCapitalized;
 }
 
-const message = 'Ye\'sterday, we "bumped" into Laura. It had to happen, but you can\'t deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month? By the way, she still: has the ring I gave her! Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first.';
+
+const message = 'Ye\'sterday, we "bumped" into Laura. It had to happen, but you can\'t deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month? By the way, she still: has the ring I gave her! Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first.'
+
+const message2 = 'Full he. Uncommonly surrounded considered hello him are its. It there is read good soon. My to. Did general dashwoods deficient man concluded additions resources. Or Kenobi packages overcame distance smallest in recurred.';
+
+const message3 = 'Death weeks early. Hearted forbade on an for ye in fifteen. Age attended betrayed her the man raptures laughter. Instrument terminated of as Horde astonished literature motionless admiration. The affect. Are determine loktar how performed intention discourse but. On merits on so valley ogar indeed assure of. Has. Particular boisterous thromka uncommonly are.'
 
 console.log(decodeMessage(message));
+console.log(decodeMessage(message2));
+console.log(decodeMessage(message3));
 
