0ab20b2aff985b276a7de9517556f323295fd3d7
Andrii <andryshonochek0604@gmail.com>
2020-02-17 17:30:50 +0200

    Add unshift polyfill

--- lesson05HW/task1/script.js
+++ lesson05HW/task1/script.js
@@ -1,3 +1,5 @@
+//work on exceptions
+
 function myPop(arr) {
   if (!arr.length) {
     return;
@@ -9,7 +11,7 @@ function myPop(arr) {
   return popedElem;
 }
 
-function push(arr, ...args) {
+function myPush(arr, ...args) {
   for (let i = 0; i < args.length; i++) {
     arr[arr.length] = args[i];
   }
@@ -24,7 +26,6 @@ function myShift(arr) {
 
   const shiftedElem = arr[0];
 
-
   for (let i = 0; i < arr.length - 1; i++) {
     arr[i] = arr[i + 1];
   }
@@ -34,11 +35,26 @@ function myShift(arr) {
   return shiftedElem;
 }
 
+function myUnshift(arr, ...args) {
+  for (let i = args.length - 1; i >= 0; i--) {
+    arr.length = arr.length + 1;
+    for (let j = arr.length - 1; j > 0; j--) {
+      arr[j] = arr[j - 1];
+    }
+    arr[0] = args[i]
+  }
+
+  return arr.length;
+}
+
 const arr1 = [1, 2, 10, 15];
 const arr2 = [];
 const arr3 = ['Hello', 'How', 'Are', 'Ya'];
 
-console.log('Default: ', arr1);
-console.log(push(arr1, 20, 30, '?'));
-console.log('Changed: ', arr1);
+// console.log('Default: ', arr1);
+// console.log(myShift());
+// console.log('Changed: ', arr1);
+
+console.log(myUnshift(arr2, 2))
+console.log(arr2)
 
