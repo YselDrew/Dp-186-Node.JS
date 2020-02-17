471cbffa3ea4a491d1679af6940670e518cde3c5
Andrii <andryshonochek0604@gmail.com>
2020-02-17 17:00:09 +0200

    Add shift polyfill

--- lesson05HW/task1/script.js
+++ lesson05HW/task1/script.js
@@ -1,19 +1,36 @@
 function myPop(arr) {
   if (!arr.length) {
-    return
+    return;
   }
 
   const popedElem = arr[arr.length - 1];
-  arr.length = arr.length - 1
+  arr.length = arr.length - 1;
 
   return popedElem;
 }
 
-const arr1 = [1, 2, 10, 15];
-const arr2 = []
+function myShift(arr) {
+  if(!arr.length) {
+    return;
+  }
 
-console.log('Default: ', arr1)
-console.log(myPop(arr1));
-console.log('Changed: ', arr1)
-console.log(myPop(arr2));
+  const shiftedElem = arr[0];
+
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+
+  arr.length = arr.length - 1;
+
+  return shiftedElem;
+}
+
+const arr1 = [1, 2, 10, 15];
+const arr2 = [];
+const arr3 = ['Hello', 'How', 'Are', 'Ya'];
+
+console.log('Default: ', arr1);
+console.log(myShift(arr1));
+console.log('Changed: ', arr1);
 
