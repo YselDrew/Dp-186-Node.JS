367f41f6a983981952526f0c2d7ec63bf18836a7
Andrii <andryshonochek0604@gmail.com>
2020-02-26 20:56:56 +0200

    Add writeFile option

--- NodeJs01HW/server.js
+++ NodeJs01HW/server.js
@@ -3,15 +3,30 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const server = http.createServer((req, res) => {
-  res.end('<h1>Home</h1>')
+  let data = '';
+
+  if(req.method.toLowerCase() === "post") {
+    req.on('data', chunk => {
+      data += chunk;
+      fs.writeFile(`./data/${uuidv4()}.txt`, JSON.stringify(data), (error) => {
+        if (error) {
+          throw error; 
+        }
+        console.log("File written succesfully");
+      })
+    })
+    res.end("Received POST request");
+  } else if (req.method.toLowerCase() === "get") {
+    res.end("Received GET request");
+  }
 })
 
 const PORT = 3000;
 
 server.listen(PORT, (error) => {
   if(error) {
-    console.log('Something went wrong', error)
+    console.log('Something went wrong', error);
   } else {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
   }
 })
