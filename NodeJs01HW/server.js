a9a2b99700a1967337184d2d1a098c0f0f37ca6c
Andrii <andryshonochek0604@gmail.com>
2020-02-26 22:20:30 +0200

    Add readfile, but it doesn't work correctly

--- NodeJs01HW/server.js
+++ NodeJs01HW/server.js
@@ -4,11 +4,28 @@ const { v4: uuidv4 } = require('uuid');
 
 const server = http.createServer((req, res) => {
   let data = '';
+  const arr = [];
+
+  if (req.method.toLowerCase() === "get") {
+    fs.readdir('./data', (error, files) => {
+      if (error) {
+        console.log('Unable to scan directory: ' + error);
+      }
+      files.forEach(file => {
+        let filePath = `./data/${file}`
+        fs.readFile(filePath, (error, data) => {
+          const fileData = data.toString('utf8');
+          // arr.push and res.end(arr) but it doesn't work without promisified fs.readFile
+        })
+      })
+    })
+    res.send('Received GET request');
+  }
 
   if(req.method.toLowerCase() === "post") {
     req.on('data', chunk => {
-      data += chunk;
-      fs.writeFile(`./data/${uuidv4()}.txt`, JSON.stringify(data), (error) => {
+      data += chunk.toString('utf8');
+      fs.writeFile(`./data/${uuidv4()}.json`, (data), (error) => {
         if (error) {
           throw error; 
         }
@@ -16,9 +33,7 @@ const server = http.createServer((req, res) => {
       })
     })
     res.end("Received POST request");
-  } else if (req.method.toLowerCase() === "get") {
-    res.end("Received GET request");
-  }
+  } 
 })
 
 const PORT = 3000;
